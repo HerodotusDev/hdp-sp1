2eac804149bdff367fbb6db4ddd1bb52e24abbd1
Bartosz Nowak <bartekn337@gmail.com>
2024-10-01 14:19:11 -0700

    wip

--- lib/src/memorizer/mod.rs
+++ lib/src/memorizer/mod.rs
@@ -1,8 +1,10 @@
 pub mod keys;
 
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-pub type Memorizer = HashMap<MemorizerKey, Vec<u8>>;
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Memorizer(pub HashMap<MemorizerKey, Vec<u8>>);
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct MemorizerKey(pub [u8; 32]);
